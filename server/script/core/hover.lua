899c402021da6799bafb627a77e08ea8b56de867
最萌小汐 <sumneko@hotmail.com>
2020-11-25 12:17:54 +0800

    支持unicode转义

---
+++ server/script/core/hover.lua
@@ -0,0 +1,82 @@
+local files = require 'files'
+
+local function findSource(text, offset)
+    for start, word, finish in string.gmatch(text, '()"([^\r\n]-)"()', math.max(1, offset - 1000)) do
+        if start <= offset and finish >= offset then
+            return {
+                type   = 'string',
+                text   = word,
+                start  = start,
+                finish = finish - 1,
+            }
+        end
+    end
+    for start, word, finish in string.gmatch(text, '()([%w_]+)()', math.max(1, offset - 1000)) do
+        if start <= offset and finish >= offset then
+            return {
+                type   = 'word',
+                text   = word,
+                start  = start,
+                finish = finish - 1,
+            }
+        end
+    end
+    return nil
+end
+
+local ESCMap = {
+    ['\\r']  = '\r',
+    ['\\n']  = '\n',
+    ['\\t']  = '\t',
+    ['\\\\'] = '\\',
+    ['\\"']  = '"'
+}
+
+local function decodeStringESC(str)
+    return str
+    : gsub('\\u(....)', function (int16)
+        return utf8.char(tonumber(int16, 16))
+    end)
+    : gsub('\\x(..)', function (char16)
+        -- TODO 遇到16进制字符时，假定前面有个 \xC2
+        return string.char(0xC2, tonumber(char16, 16))
+    end)
+    : gsub('\\.', ESCMap)
+end
+
+local function getLabelAsString(source)
+    local newText = decodeStringESC(source.text)
+    if newText ~= source.text then
+        return newText
+    end
+end
+
+local function getLabel(source)
+    if source.type == 'string' then
+        -- 字符串
+        return getLabelAsString(source)
+    end
+end
+
+local function hoverbyUri(uri, offset)
+    local text = files.getText(uri)
+    if not text then
+        return
+    end
+    local source = findSource(text, offset)
+    if not source then
+        return
+    end
+    local label = getLabel(source)
+    if not label then
+        return
+    end
+    return {
+        label       = label,
+        source      = source,
+    }
+end
+
+return {
+    byUri = hoverbyUri,
+}
--- server/script/provider/provider.lua
+++ server/script/provider/provider.lua
@@ -74,20 +74,48 @@ proto.on('workspace/didChangeConfiguration', function ()
 end)
 
 proto.on('textDocument/didOpen', function (params)
-    local doc   = params.textDocument
-    local uri   = doc.uri
-    local text  = doc.text
-    files.open(uri)
-    files.setText(uri, text)
 end)
 
 proto.on('textDocument/didClose', function (params)
-    local doc   = params.textDocument
-    local uri   = doc.uri
-    files.close(uri)
-    files.remove(uri)
+end)
+
+proto.on('textDocument/didChange', function (params)
+    local doc    = params.textDocument
+    local change = params.contentChanges
+    local uri    = doc.uri
+    local text   = change[1].text
+    files.setText(uri, text)
+    log.debug('didChange', uri)
 end)
 
 proto.on('textDocument/hover', function (params)
-    log.debug(util.dump(params))
+    await.close 'hover'
+    await.setID 'hover'
+    local core = require 'core.hover'
+    local doc    = params.textDocument
+    local uri    = doc.uri
+    if not files.exists(uri) then
+        local text = util.loadFile(furi.decode(uri))
+        if not text then
+            return
+        end
+        files.setText(uri, text)
+    end
+    local lines  = files.getLines(uri)
+    local text   = files.getText(uri)
+    local offset = define.offsetOfWord(lines, text, params.position)
+    local hover = core.byUri(uri, offset)
+    if not hover then
+        return nil
+    end
+    local md = markdown()
+    md:add('md', hover.label)
+    md:add('md', hover.description)
+    return {
+        contents = {
+            value = md:string(),
+            kind  = 'markdown',
+        },
+        range = define.range(lines, text, hover.source.start, hover.source.finish),
+    }
 end)
