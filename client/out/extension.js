2edcb8af18b7d1ef75dadb9c481ca7e17d829aa2
最萌小汐 <sumneko@hotmail.com>
2022-04-21 15:38:58 +0800

    remove patch

--- client/out/extension.js
+++ client/out/extension.js
@@ -2,10 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const languageserver = require("./languageserver");
-const debuggerPatch = require("./debuggerPatch");
 function activate(context) {
     languageserver.activate(context);
-    debuggerPatch.applyPatch(context);
 }
 exports.activate = activate;
 function deactivate() {
